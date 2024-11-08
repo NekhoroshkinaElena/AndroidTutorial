8094ccad15b5561390b14a318f956093b5f62042
NekhoroshkinaElena <muxaewaelena225@yandex.ru>
2024-11-08 17:54:34 +0300

    fix: избавиться от лишних полей во ViewModels

--- app/src/main/java/com/example/androidtutorial2/repeat/ui/RepeatViewModel.kt
+++ app/src/main/java/com/example/androidtutorial2/repeat/ui/RepeatViewModel.kt
@@ -3,30 +3,20 @@ package com.example.androidtutorial2.repeat.ui
 import androidx.lifecycle.viewModelScope
 import com.example.androidtutorial2.base.BaseViewModel
 import com.example.androidtutorial2.themes.domain.ThemesInteractor
-import com.example.androidtutorial2.themes.domain.model.Theme
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RepeatViewModel @Inject constructor(private val themesInteractor: ThemesInteractor) :
     BaseViewModel<RepeatScreenState>(RepeatScreenState.Loading) {
 
-    private var listTheme: List<Theme> = emptyList()
-    private val themesMutex = Mutex()
-
     init {
         viewModelScope.launch(Dispatchers.IO) {
             val themes = themesInteractor.getListThemes()
 
-            themesMutex.withLock {
-                listTheme = themes
-            }
-
             withContext(Dispatchers.Main) {
-                updateScreenState(RepeatScreenState.Content(listTheme))
+                updateScreenState(RepeatScreenState.Content(themes))
             }
         }
     }
--- app/src/main/java/com/example/androidtutorial2/sub_themes/ui/SubThemesViewModel.kt
+++ app/src/main/java/com/example/androidtutorial2/sub_themes/ui/SubThemesViewModel.kt
@@ -2,12 +2,9 @@ package com.example.androidtutorial2.sub_themes.ui
 
 import androidx.lifecycle.viewModelScope
 import com.example.androidtutorial2.base.BaseViewModel
-import com.example.androidtutorial2.sub_themes.domain.SubTheme
 import com.example.androidtutorial2.sub_themes.domain.SubThemesInteractor
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -15,16 +12,9 @@ class SubThemesViewModel @Inject constructor(
     private val subThemesInteractor: SubThemesInteractor
 ) : BaseViewModel<SubThemesScreenState>(SubThemesScreenState.Loading) {
 
-    private var listSubThemes: List<SubTheme> = emptyList()
-    private val subThemesMutex = Mutex()
-
     fun getSubThemes(themeId: Int) {
         viewModelScope.launch(Dispatchers.IO) {
             val subThemes = subThemesInteractor.getSubThemes(themeId)
-
-            subThemesMutex.withLock {
-                listSubThemes = subThemes
-            }
 
             withContext(Dispatchers.Main) {
                 updateScreenState(SubThemesScreenState.Content(subThemes))
