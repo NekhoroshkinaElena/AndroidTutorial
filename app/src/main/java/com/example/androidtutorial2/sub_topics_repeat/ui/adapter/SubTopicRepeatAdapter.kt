3d11f20318eb87d06204f66f743b822dfd018776
NekhoroshkinaElena <muxaewaelena225@yandex.ru>
2025-02-05 10:58:57 +0300

    refactor: строки вынесены в ресурсы refactor: tost заменён на snackbar

--- app/src/main/java/com/example/androidtutorial2/sub_topics_repeat/ui/adapter/SubTopicRepeatAdapter.kt
+++ app/src/main/java/com/example/androidtutorial2/sub_topics_repeat/ui/adapter/SubTopicRepeatAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.androidtutorial2.sub_topics_repeat.ui.adapter
 
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidtutorial2.sub_topics.domain.model.SubTopic
 
@@ -24,11 +23,7 @@ class SubTopicRepeatAdapter(
         holder.bind(subTopic)
 
         holder.itemView.setOnClickListener {
-            if (subTopic.isSelected){
-                onClick(subTopic)
-            } else {
-                Toast.makeText(holder.itemView.context, "начните повторять, что бы перейти к материалу", Toast.LENGTH_SHORT).show()
-            }
+            onClick(subTopic)
         }
 
         holder.setMenuItemClickListener { itemId ->
